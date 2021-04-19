94077febdc410fa0fdc5a80d52c81a5fde103e71
ben <bbclr20@gmail.com>
2021-04-19 16:19:32 +0800

    add exit handlers

---
+++ examples/chp25/25_1_exit_handlers/exit_handlers.c
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+static void
+atexitFunc1(void) {
+    /* Get on_exit() declaration from <stdlib.h> */
+    printf("atexit function 1 called\n");
+}
+
+static void
+atexitFunc2(void) {
+    printf("atexit function 2 called\n");
+}
+
+static void
+onexitFunc(int exitStatus, void *arg) {
+    printf("on_exit function called: status=%d, arg=%ld\n", exitStatus, (long) arg);
+}
+
+int main(int argc, char *argv[]){
+    // if (on_exit(onexitFunc, (void *) 10) != 0) {
+    //     exit(-1);
+    // }
+    
+    if (atexit(atexitFunc1) != 0) {
+        exit(-1);
+    }
+
+    if (atexit(atexitFunc2) != 0) {
+        exit(-1);
+    }
+
+    // if (on_exit(onexitFunc, (void *) 20) != 0) {
+    //     exit(-1);
+    // }
+    exit(2);
+}
